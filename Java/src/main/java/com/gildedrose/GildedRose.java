dcc4edf34ef2851cbec2e5e03d88424cc1607e0d
104 Harry Lu <harry.nan.825@gmail.com>
2020-09-01 09:29:19 +0800

    fixed

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -36,7 +36,7 @@ class GildedRose {
                     increaseQuality(item);
                 } else {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     } else {
                         if (item.quality > 0) {
                             item.quality = item.quality - 1;
